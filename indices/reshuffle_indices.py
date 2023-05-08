f154b0c1a7b3721b6d7b2d67c27b13e60d77bb96
maxahoy <maxahoy@gmail.com>
2023-05-07 22:16:01 -0400

    Added a bunch of model files, reshuffle script, and changes to main to allow for sex prediction

---
+++ indices/reshuffle_indices.py
@@ -0,0 +1,59 @@
+#create new shuffles of the testing and training indices
+import os
+import random
+
+indices_type = 'birth_age'
+#or 'scan_age' or 'sex'
+input_folder = '.'
+full_file_name = indices_type + "_full.txt"
+train_file_name = indices_type + "_train_reshuffle.txt"
+test_file_name = indices_type + "_test_reshuffle.txt"
+val_file_name = indices_type + "_val_reshuffle.txt"
+
+#create file paths
+full_path = os.path.join(input_folder, full_file_name)
+print(full_path)
+
+train_path = os.path.join(input_folder, train_file_name)
+test_path = os.path.join(input_folder, test_file_name)
+val_path = os.path.join(input_folder, val_file_name)
+
+full_list = open(full_path).read().split('\n')
+
+list_len = len(full_list)
+sample_size = list_len / 3
+
+#reorder list randomly to act as sample without replacement
+shuffle_list = random.shuffle(full_list)
+
+#select first third as training, next third as testing, next third as val
+new_train_list = []
+new_test_list = []
+new_val_list = []
+
+third = 0
+
+for i in range(list_len):
+    entry = full_list[i]
+
+    third = i % 3
+    if third == 0:
+        new_train_list.append(entry)
+    elif third == 1:
+        new_test_list.append(entry)
+    elif third == 2:
+        new_val_list.append(entry)
+
+train_write = open(train_file_name, 'w+')
+test_write = open(test_file_name, 'w+')
+val_write = open(val_file_name, 'w+')
+
+train_write.writelines([f"{line}\n" for line in new_train_list if line])
+print("Wrote ", len(new_train_list), " entries to ", train_file_name)
+
+test_write.writelines([f"{line}\n" for line in new_test_list if line])
+print("Wrote ", len(new_test_list), " entries to ", test_file_name)
+
+val_write.writelines([f"{line}\n" for line in new_val_list if line])
+print("Wrote ", len(new_val_list), " entries to ", val_file_name)
+
--- train_test.py
+++ train_test.py
@@ -29,3 +29,38 @@ def test_model(test_loader, model):
             test_acc = F.l1_loss(y_pred, y_true)
             test_accs.append(test_acc)
     return sum(test_accs) / len(test_accs)
+
+def display_test_stats(test_loader, model):
+    model.eval()
+    test_accs = []
+    print("This ran")
+    predictions = []
+    truths = []
+    with torch.no_grad():
+        for test_data in test_loader:
+            x, y_true = test_data
+            truths.append(y_true)
+            x = x.to(model.device)
+            y_true = y_true.to(model.device)
+            y_pred = model(x) #todo; see what this is predicting by usinjg printouts, can it predict a class 1 or 0?
+            # can another one be trained to try and predict birth sex?
+            #print('y_pred', y_pred)
+            predictions.append(y_pred)
+            
+
+
+            #predictions.append(y_pred)
+            
+
+            test_acc = F.l1_loss(y_pred, y_true) #gets the mean element-wise absolute value difference;
+            # this 
+            test_accs.append(test_acc)
+    #stats we want; average accuracy (sum of accs / len of accs)
+
+    #we want to update this code so that it spits back the accuracy of the scanned ages and birth ages
+    #
+    # 
+    mae = F.l1_loss(y_pred, y_true)
+    return (sum(test_accs) / len(test_accs)), predictions, truths, mae
+
+
